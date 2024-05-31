9f35effbfe8d55cac6b04851283f06ce2b282abf
LORENZO STEFANONI s330591 <s330591@studenti.polito.it>
2024-05-31 14:42:08 +0200

    third product commit

--- code/server/src/dao/productDAO.ts
+++ code/server/src/dao/productDAO.ts
@@ -1,119 +1,145 @@
-import db from "../db/db"
-import { Product } from "../components/product"
+import db from "../db/db";
+import { Product } from "../components/product";
+import dayjs from 'dayjs';
+import { ProductAlreadyExistsError, ProductNotFoundError, LowProductStockError, EmptyProductStockError } from "../errors/productError";
+import { DateError } from "../utilities";
 
 /**
  * A class that implements the interaction with the database for all product-related operations.
  * You are free to implement any method you need here, as long as the requirements are satisfied.
  */
-
 class ProductDAO {
 
     /** 
      * It creates a new model in the products table. If the products table doesn't exist, it will create it and put the new model in. 
      * It doesn't return anything
-    */
+     */
     newModel(model: string, category: string, quantity: number, details: string | null, sellingPrice: number, arrivalDate: string | null): Promise<void> {
         return new Promise((resolve, reject) => {
-            try {
-                const createTableSql = 
-                    `CREATE TABLE IF NOT EXISTS products (
-                    model TEXT PRIMARY KEY,
-                    category TEXT,
-                    quantity INTEGER,
-                    details TEXT,
-                    sellingPrice REAL,
-                    arrivalDate TEXT)`;
-                this.runSql(createTableSql).then(() => {
+
+            // Verify the date
+            if (arrivalDate === null) arrivalDate = dayjs().format("YYYY-MM-DD");
+            else if (dayjs(arrivalDate).isAfter(dayjs())) return reject(new DateError());
+            
+            // Create table if it doesn't exist
+            const createTableSql = 
+                `CREATE TABLE IF NOT EXISTS products (
+                model TEXT PRIMARY KEY,
+                category TEXT,
+                quantity INTEGER,
+                details TEXT,
+                sellingPrice REAL,
+                arrivalDate TEXT)`;
+
+            // Runs the query
+            this.runSql(createTableSql)
+                .then(() => {
                     const insertSql = "INSERT INTO products (model, category, quantity, details, sellingPrice, arrivalDate) VALUES (?, ?, ?, ?, ?, ?)";
                     return this.runSql(insertSql, [model, category, quantity, details, sellingPrice, arrivalDate]);
-                }).then(resolve).catch(err => {
-                    reject(new Error(`Error in newModel: ${err.message}`));
+                })
+                .then(resolve)
+                .catch(err => {
+                    reject(new ProductAlreadyExistsError());
                 });
-            } catch (err) {
-                reject(new Error(`Error in newModel: ${err.message}`));
-            }
         });
     }
 
     /** 
-     * It update a model quantity and/or arrival date.
+     * It updates a model's quantity and/or arrival date.
      * The number to use as quantity is not the new quantity but the increment to add to the old one.
-     * It doens't return anything
-    */
+     * It doesn't return anything
+     */
     updateModel(model: string, quantity: number, arrivalDate: string | null): Promise<void> {
         return new Promise((resolve, reject) => {
-            try {
-                const sql = "UPDATE products SET quantity = quantity + ?, arrivalDate = ? WHERE model = ?";
-                this.runSql(sql, [quantity, arrivalDate, model]).then(resolve).catch(err => {
-                    reject(new Error(`Error in updateModel: ${err.message}`));
+
+            // Checks the date
+            if (arrivalDate === null) arrivalDate = dayjs().format("YYYY-MM-DD");
+            else if (dayjs(arrivalDate).isAfter(dayjs())) return reject(new DateError());
+
+            const sql = "SELECT arrivalDate FROM products WHERE model = ?";
+            this.getSql(sql, [model])
+                .then((row: { arrivalDate: string }) => {
+                    if (!row) return reject(new ProductNotFoundError());
+                    if (dayjs(arrivalDate).isBefore(dayjs(row.arrivalDate))) return reject(new DateError());
+                    arrivalDate = dayjs(arrivalDate).format("YYYY-MM-DD");
+
+                    const updateSql = "UPDATE products SET quantity = quantity + ?, arrivalDate = ? WHERE model = ?";
+                    return this.runSql(updateSql, [quantity, arrivalDate, model]);
+                })
+                .then(resolve)
+                .catch(err => {
+                    reject(err);
                 });
-            } catch (err) {
-                reject(new Error(`Error in updateModel: ${err.message}`));
-            }
         });
     }
 
     /** 
-     * It allows to sell products. Model is the name of the model sold, quantity is the number of products sold, selling date is
-     * the date when it happened the selling. If the selling date is null, the date used is the one of today.
-     * It returns the number of product remainings
-    */
+     * It allows selling products. Model is the name of the model sold, quantity is the number of products sold, selling date is
+     * the date when it happened. If the selling date is null, the date used is the one of today.
+     * It returns the number of products remaining.
+     */
     sellModel(model: string, quantity: number, sellingDate: string | null): Promise<number> {
         return new Promise((resolve, reject) => {
-            try {
-                const createProductsHistoryTableSql = 
-                    `CREATE TABLE IF NOT EXISTS products_history (
-                    id INTEGER PRIMARY KEY AUTOINCREMENT,
-                    model TEXT,
-                    quantity INTEGER,
-                    sellingDate TEXT)`;
-                this.runSql(createProductsHistoryTableSql).then(() => {
-                    const updateSql = "UPDATE products SET quantity = quantity - ? WHERE model = ?";
-                    return this.runSql(updateSql, [quantity, model]);
-                }).then(() => {
-                    const insertHistorySql = "INSERT INTO products_history (model, quantity, sellingDate) VALUES (?, ?, ?)";
-                    return this.runSql(insertHistorySql, [model, quantity, sellingDate]);
-                }).then(() => {
-                    const getQuantitySql = "SELECT quantity FROM products WHERE model = ?";
-                    return this.getSql(getQuantitySql, [model]);
-                }).then(row => {
-                    resolve(row.quantity);
-                }).catch(err => {
-                    reject(new Error(`Error in sellModel: ${err.message}`));
+
+            // Checks the date
+            if (sellingDate === null) sellingDate = dayjs().format("YYYY-MM-DD");
+            else if (dayjs(sellingDate).isAfter(dayjs())) return reject(new DateError());
+
+            const sql = "SELECT arrivalDate, quantity FROM products WHERE model = ?";
+            this.getSql(sql, [model])
+                .then((row: { arrivalDate: string, quantity: number }) => {
+                    if (!row) return reject(new ProductNotFoundError());
+                    if (dayjs(sellingDate).isBefore(dayjs(row.arrivalDate))) return reject(new DateError());
+                    if (row.quantity === 0) return reject(new EmptyProductStockError());
+                    if (row.quantity < quantity) return reject(new LowProductStockError());
+
+                    const updateSql = "UPDATE products SET quantity = quantity - ?, arrivalDate = ? WHERE model = ?";
+                    return this.runSql(updateSql, [quantity, sellingDate, model]).then(() => resolve(row.quantity - quantity));
+                })
+                .catch(err => {
+                    reject(err);
                 });
-            } catch (err) {
-                reject(new Error(`Error in sellModel: ${err.message}`));
-            }
         });
     }
 
     /** 
      * It gets all the products. They can be filtered by category (one between "Smartphone", "Laptop", "Appliance") or by model. If so,
      * "grouping" should be equal to the word "category" or "model"
-    */
+     */
     getAllProducts(grouping: string | null, category: string | null, model: string | null): Promise<Product[]> {
         return new Promise((resolve, reject) => {
             try {
                 let sql = "SELECT * FROM products";
-                let param;
+                let params: any[] = [];
 
+                // Checks errors and sets query parameters
                 if (grouping) {
-                    if (grouping === "category" && category != null) {
+                    if (grouping === "category" && category && !model) {
                         sql += " WHERE category = ?";
-                        param = [category];
-                    } else if (grouping === "model" && model != null) {
+                        params = [category];
+                    } else if (grouping === "model" && model && !category) {
                         sql += " WHERE model = ?";
-                        param = [model];
+                        params = [model];
+                    } else {
+                        return reject(new Error("Invalid parameters"));
+                    }
+                } else {
+                    if (model || category) {
+                        return reject(new Error("Invalid parameters"));
                     }
                 }
 
-                this.allSql(sql, param).then(rows => {
-                    resolve(rows);
-                }).catch(err => {
-                    reject(new Error(`Error in getAllProducts: ${err.message}`));
-                });
+                // Runs the query
+                this.allSql(sql, params)
+                    .then((rows) => {
+                        if (rows.length === 0 && model) return reject(new ProductNotFoundError());
+                        resolve(rows);
+                    })
+                    .catch(err => {
+                        reject(err);
+                    });
             } catch (err) {
-                reject(new Error(`Error in getAllProducts: ${err.message}`));
+                reject(err);
             }
         });
     }
@@ -125,16 +151,13 @@ class ProductDAO {
         return new Promise((resolve, reject) => {
             try {
                 const sqlDeleteProducts = "DELETE FROM products";
-                this.runSql(sqlDeleteProducts).then(() => {
-                    const sqlDeleteProductsHistory = "DELETE FROM products_history";
-                    return this.runSql(sqlDeleteProductsHistory);
-                }).then(() => {
-                    resolve(true);
-                }).catch(err => {
-                    reject(new Error(`Error in deleteAllProducts: ${err.message}`));
-                });
+                this.runSql(sqlDeleteProducts)
+                    .then(() => resolve(true))
+                    .catch(err => {
+                        reject(err);
+                    });
             } catch (err) {
-                reject(new Error(`Error in deleteAllProducts: ${err.message}`));
+                reject(err);
             }
         });
     }
@@ -142,22 +165,19 @@ class ProductDAO {
     /** 
      * It deletes a specific product from the products table based on the model.
      * It also deletes the corresponding entries from the products_history table.
-     * It returns true if the operation is successful
-    */
+     * It returns true if the operation is successful.
+     */
     deleteOneProduct(model: string): Promise<boolean> {
         return new Promise((resolve, reject) => {
             try {
                 const sql = "DELETE FROM products WHERE model = ?";
-                this.runSql(sql, [model]).then(() => {
-                    const sqlHistory = "DELETE FROM products_history WHERE model = ?";
-                    return this.runSql(sqlHistory, [model]);
-                }).then(() => {
-                    resolve(true);
-                }).catch(err => {
-                    reject(new Error(`Error in deleteOneProduct: ${err.message}`));
-                });
+                this.runSql(sql, [model])
+                    .then(() => resolve(true))
+                    .catch(err => {
+                        reject(err);
+                    });
             } catch (err) {
-                reject(new Error(`Error in deleteOneProduct: ${err.message}`));
+                reject(err);
             }
         });
     }
