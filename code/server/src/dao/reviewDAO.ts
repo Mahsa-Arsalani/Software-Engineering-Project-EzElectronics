4c9a2c5f763349e554cd5fb32371fba8c2e54c04
MERIC ULUCAY S328899 <s328899@studenti.polito.it>
2024-05-31 12:11:01 +0200

    update reviewdao 3

--- code/server/src/dao/reviewDAO.ts
+++ code/server/src/dao/reviewDAO.ts
@@ -1,6 +1,6 @@
 import db from "../db/db"
-import { User} from "../components/user"
-import { ExistingReviewError} from "../errors/reviewError";
+import { User, Review} from "../components/user"
+import { ExistingReviewError, NoReviewProductError} from "../errors/reviewError";
 /**
  * A class that implements the interaction with the database for all review-related operations.
  * You are free to implement any method you need here, as long as the requirements are satisfied.
@@ -20,16 +20,16 @@ addReview(model:string,user:User,score:number,comment:string):Promise<void>{
             const checkSql = "SELECT COUNT(*) AS count FROM reviews WHERE user = ? AND model = ?";
             db.get(checkSql, [user, model], (err: Error | null, row: any) => {
                 if (err) {
-                    return reject(err);
+                    reject(err);
                 }
                 if (row.count > 0) {
-                    return reject(new ExistingReviewError());
+                    reject(new ExistingReviewError());
                 }
                 const date = new Date().toISOString().split('T')[0];
                 const insertsql="INSERT INTO reviews(user,model,score,date, comment) VALUES (?,?,?,?,?)";
             db.run(insertsql, [user,model,score,date,comment], (err: Error | null) => {
                 if (err) {
-                    return reject(err);
+                    reject(err);
                 }
                 resolve();
             });
@@ -39,6 +39,33 @@ addReview(model:string,user:User,score:number,comment:string):Promise<void>{
     }
 });
 }
+ /**
+     * Returns all reviews for a product
+     * @param model The model of the product to get reviews from
+     * @returns A Promise that resolves to an array of ProductReview objects
+     */
+ getProductReviews(model: string):Promise<ProductReview[]>{ 
+    return new Promise<ProductReview>((resolve,reject)=>{
+        try{
+            const sql="SELECT * FROM reviews WHERE model = ?"
+            db.get(sql,[model], (err:Error | null,row: any)=> {
+                if (err) {
+                    reject(err)
+                    return
+                }
+                if(!row){
+                    reject(new NoReviewProductError())
+                    return
+                }
+                const reviews: Review[] = rows.map((row:any) =>new Review(row.score, row.date, row.comment))
+                resolve(reviews)
+            })
+        } catch (error){
+            reject(error)
+        }
+    })
+ }
+ 
 
 }
 
