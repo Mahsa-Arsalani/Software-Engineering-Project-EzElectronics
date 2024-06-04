43aaddb62b04dbd749c46a7f1b50327f36caeece
Mahsa Arsalani <s327617@studenti.polito.it>
2024-06-04 21:48:20 +0200

    Update cartDAO.ts

--- code/server/src/dao/cartDAO.ts
+++ code/server/src/dao/cartDAO.ts
@@ -1,39 +1,13 @@
 import db from "../db/db"
 import {Cart, ProductInCart} from "../components/cart";
 import {User, Role} from "../components/user";
-import {CartNotFoundError, ProductInCartError, ProductNotInCartError, WrongUserCartError, EmptyCartError} from "../errors/cartError";
+import {CartNotFoundError} from "../errors/cartError";
 import {UserNotCustomerError} from "../errors/userError";
 /**
  * A class that implements the interaction with the database for all cart-related operations.
  * You are free to implement any method you need here, as long as the requirements are satisfied.
  */
 class CartDAO {
-/**
- * Retrieves the current cart for a specific user.
- * @param user - The user for whom to retrieve the cart.
- * @returns A Promise that resolves to the user's cart or an empty one if there is no current cart.
- */
-getCart(user: User) : Promise<Cart> {
-    return new Promise<Cart>((resolve, reject) => {
-        try {
-            const username = user.username;
-            const sql = "SELECT * FROM cart WHERE customer = ? AND paid = 0"
-            db.get(sql, [username], (err: Error | null, row: any) => {
-                if (err) 
-                    reject(err)
-                if (!row) {
-                    reject(new CartNotFoundError);
-                } else {
-                    const cart: Cart = new Cart(row.customer, row.paid, row.paymentDate, row.total, row.products);
-                    resolve(cart)
-                }
-            })
-        } catch (error) {
-            reject(error);
-        }
-    })
-}   
-
 
 /**
  * Adds a product to the user's cart. If the product is already in the cart, the quantity should be increased by 1.
@@ -138,13 +112,39 @@ addToCart(user : User, product : string):Promise<Boolean>{
 
 
 
+/**
+ * Retrieves the current cart for a specific user.
+ * @param user - The user for whom to retrieve the cart.
+ * @returns A Promise that resolves to the user's cart or an empty one if there is no current cart.
+ */
+getCart(user: User) : Promise<Cart> {
+    return new Promise<Cart>((resolve, reject) => {
+        try {
+            const username = user.username;
+            const sql = "SELECT * FROM cart WHERE customer = ? AND paid = 0"
+            db.get(sql, [username], (err: Error | null, row: any) => {
+                if (err) 
+                    reject(err)
+                if (!row) {
+                    reject(new CartNotFoundError);
+                } else {
+                    const cart: Cart = new Cart(row.customer, row.paid, row.paymentDate, row.total, row.products);
+                    resolve(cart)
+                }
+            })
+        } catch (error) {
+            reject(error);
+        }
+    })
+}   
+
 
 
 /**
  * Checks out the user's cart. We assume that payment is always successful, there is no need to implement anything related to payment.
  * @param user - The user whose cart should be checked out.
  * @returns A Promise that resolves to `true` if the cart was successfully checked out.
- * 
+ * ******* It fails if the cart is empty, there is no current cart in the database, or at least one of the products in the cart is not available in the required quantity.
  */
 checkoutCart(user: User):Promise<Boolean>{
     return new Promise<Boolean>((resolve, reject)=>{
@@ -164,10 +164,24 @@ checkoutCart(user: User):Promise<Boolean>{
  * @param user - The customer for whom to retrieve the carts.
  * @returns A Promise that resolves to an array of carts belonging to the customer.
  * Only the carts that have been checked out should be returned, the current cart should not be included in the result.
+ * (only carts that have been paid for are returned - the current cart is not included in the list).
  */
 async getCustomerCarts(user: User):Promise<Cart[]> {
     return new Promise<Cart[]>((resolve, reject)=>{
         try{
+            const username = user.username;
+            const sql = "SELECT * FROM cart WHERE customer = ? AND paid = 1"
+            db.all(sql, [username], (err:Error | null, rows: any) => {
+                if(err) {
+                    reject(err);
+                    return
+                }
+                if (!rows) {
+                    reject(new CartNotFoundError());
+                }
+                const carts: Cart[] = rows.map((row:any) => new Cart(row.customer, row.paid, row.paymentDate, row.total, row.products));
+                resolve(carts);
+            })
             
         } catch(error) {
             reject(error)
@@ -187,6 +201,7 @@ async getCustomerCarts(user: User):Promise<Cart[]> {
 removeProductFromCart(user: User, product: string):Promise<Boolean> {
     return new Promise<Boolean>((resolve, reject)=>{
         try{
+            // same as add to cart
             
         } catch(error) {
             reject(error)
@@ -204,6 +219,14 @@ removeProductFromCart(user: User, product: string):Promise<Boolean> {
 clearCart(user: User):Promise<Boolean> { 
     return new Promise<Boolean>((resolve, reject)=>{
         try{
+            const sql = "UPDATE cart SET products = ? WHERE paid = 0"
+            db.run(sql, [], (err:Error | null,rows: any) => {
+                if (err) {
+                    reject(err)
+                } else {
+                    resolve(true)
+                }
+            })
             
         } catch(error) {
             reject(error)
@@ -223,7 +246,13 @@ clearCart(user: User):Promise<Boolean> {
 deleteAllCarts():Promise<Boolean> { 
     return new Promise((resolve, reject)=>{
         try{
-            
+            const sql = "DELETE FROM cart";
+            db.run(sql, (err:Error | null,rows: any) => {
+                if(err) {
+                    reject(err);
+                }
+                resolve(true);
+            })   
         } catch(error) {
             reject(error)
         }
@@ -240,7 +269,19 @@ deleteAllCarts():Promise<Boolean> {
 getAllCarts():Promise<Cart[]> {
     return new Promise<Cart[]>((resolve, reject)=>{
         try{
-            
+            const sql = "SELECT * FROM cart"
+            db.all(sql, (err:Error | null, rows: any) => {
+                if(err) {
+                    reject(err);
+                    return
+                }if(!rows) {
+                    reject (new CartNotFoundError)
+                    return
+                } else {
+                    const carts: Cart[] = rows.map((row:any) => new Cart(row.customer, row.paid, row.paymentDate, row.total, row.products));
+                    resolve(carts)
+                }
+            })           
         } catch(error) {
             reject(error)
         }
@@ -251,5 +292,7 @@ getAllCarts():Promise<Cart[]> {
 
 
 }
+
+
 
 export default CartDAO
