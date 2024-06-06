cf51867a21f25570293cb99c2a562213d3ec0c10
MERIC ULUCAY S328899 <s328899@studenti.polito.it>
2024-06-06 18:46:12 +0200

    update review routes test 2

--- code/server/test_unit/route_tests/review.test.ts
+++ code/server/test_unit/route_tests/review.test.ts
@@ -1,56 +1,129 @@
-import { test, expect, jest } from "@jest/globals"
+import { test, expect, jest,afterEach,beforeEach } from "@jest/globals"
 import request from 'supertest'
 import { app } from "../../index"
 import {Role, User} from "../../src/components/user"
 import Authenticator from "../../src/routers/auth"
 import ErrorHandler from "../../src/helper"
-
 import ReviewController from "../../src/controllers/reviewController"
+import { ProductReview } from "../../src/components/review";
+import { ProductNotFoundError } from "../../src/errors/productError";
+import { NoReviewProductError, ExistingReviewError } from "../../src/errors/reviewError";
+import {UnauthorizedUserError} from "../../src/errors/userError";
+
+
 const baseURL = "/ezelectronics"
 
 //jest.mock("../../src/helper")
-jest.mock("../../src/components/review")
+//jest.mock("../../src/components/review")
 jest.mock("../../src/routers/auth")
+
+const dummyMiddleware = (req: any, res: any, next: any) => next()
+
+beforeEach(()=>{
+    jest.spyOn(ErrorHandler.prototype, "validateRequest").mockImplementation(dummyMiddleware)
+    jest.spyOn(Authenticator.prototype, "isLoggedIn").mockImplementation(dummyMiddleware)
+})
 
 afterEach(()=>{
     jest.resetAllMocks()
     jest.clearAllMocks()
 })
 
-describe("Review Routes unit test", ()=>{
-
-    const testReview1 = {
-        model: "Alexa",
-        user: "testuser1",
-        date: "2024/06/04",
-        score: 2,
-        comment: "testcomment1"
-    }
-
-    describe("POST /reviews/:model",()=>{
-
-        const testReview2 = { //Define a test review object sent to the route
-            model: "Alexa",
-            user: "testuser2",
-            date: "2024/06/04",
-            score: 3,
-            comment: "testcomment2"
-        }
-
+const testuser1=new User("testuser1","testname1","testsurname1",Role.CUSTOMER,"testaddress1","testbirthdate1")
+const testreview1 = new ProductReview("iPhone13",testuser1.username,3,"testdate1","testcomment1")
+const testreview2 = {
+    model: "iPhone13",
+    user: new User("testuser2", "testname2", "testsurname2", Role.CUSTOMER, "testaddress2", "testbirthdate2"), // User object
+    date: "testdate2",
+    score: 3,
+    comment: "testcomment2"
+}
         //Example of a unit test for the POST ezelectronics/reviews/:model route
         //The test checks if the route returns a 200 success code
         //The test also expects the createUser method of the controller to be called once with the correct parameters
-        test("It should return a 200 success code", async () => {
-            jest.spyOn(ReviewController.prototype, "addReview").mockResolvedValueOnce() //Mock the addReview method of the controller
-            const response = await request(app).post(baseURL + "/review").send(testReview2) //Send a POST request to the route
-            expect(response.status).toBe(200) //Check if the response status is 200
-            expect(ReviewController.prototype.addReview).toHaveBeenCalledTimes(1) //Check if the addReview method has been called once
-            //Check if the addReview method has been called with the correct parameters
-            expect(ReviewController.prototype.addReview).toHaveBeenCalledWith(testReview2.model,
-                testReview2.user,
-                testReview2.score,
-                testReview2.comment)
-        })
-    })
-
+test("It should return a 200 success code", async () => {
+    jest.spyOn(Authenticator.prototype,"isCustomer").mockImplementation((req,res,next)=>next());
+    jest.spyOn(ReviewController.prototype, "addReview").mockResolvedValueOnce() //Mock the addReview method of the controller
+    const response = await request(app).post('${baseURL}/reviews/iPhone13').send({ score: testreview2.score, comment: testreview2.comment });
+    expect(response.status).toBe(200) //Check if the response status is 200
+    expect(Authenticator.prototype.isCustomer).toHaveBeenCalledTimes(1);
+    expect(ReviewController.prototype.addReview).toHaveBeenCalledTimes(1) //Check if the addReview method has been called once
 })
+test("It should return a 404 status code", async () => {
+    jest.spyOn(Authenticator.prototype,"isCustomer").mockImplementation((req,res,next)=>next());
+    jest.spyOn(ReviewController.prototype, "addReview").mockRejectedValue(new ProductNotFoundError()); //Mock the addReview method of the controller
+    const response = await request(app).post('${baseURL}/reviews/notexisting').send({ score: testreview2.score, comment: testreview2.comment });
+    expect(response.status).toBe(404) //Check if the response status is 404
+    expect(response.body.error).toBe("Product not found");
+    expect(Authenticator.prototype.isCustomer).toHaveBeenCalledTimes(1);
+    expect(ReviewController.prototype.addReview).toHaveBeenCalledTimes(1) //Check if the addReview method has been called once
+})
+test("It should return a 409 status code", async () => {
+    jest.spyOn(Authenticator.prototype,"isCustomer").mockImplementation((req,res,next)=>next());
+    jest.spyOn(ReviewController.prototype, "addReview").mockRejectedValue(new ExistingReviewError()); //Mock the addReview method of the controller
+    const response = await request(app).post('${baseURL}/reviews/iPhone13').send({ score: testreview2.score, comment: testreview2.comment });
+    expect(response.status).toBe(409) //Check if the response status is 404
+    expect(response.body.error).toBe("You have already reviewed this product");
+    expect(Authenticator.prototype.isCustomer).toHaveBeenCalledTimes(1);
+    expect(ReviewController.prototype.addReview).toHaveBeenCalledTimes(1) //Check if the addReview method has been called once
+})
+//Example of a unit test for the GET ezelectronics/reviews/:model route
+test("It should return a 200 success code", async () => {
+    jest.spyOn(ReviewController.prototype, "getProductReviews").mockResolvedValueOnce([testreview1]);
+    const response = await request(app).get('${baseURL}/reviews/iPhone13');
+    expect(response.status).toBe(200);
+    expect(response.body).toEqual([testreview1]);
+    expect(ReviewController.prototype.getProductReviews).toHaveBeenCalledTimes(1);
+});
+test("It should return a 404 status code", async () => {
+    jest.spyOn(ReviewController.prototype, "getProductReviews").mockRejectedValue(new NoReviewProductError());
+    const response = await request(app).get('${baseURL}/reviews/iPhone13');
+    expect(response.status).toBe(404);
+    expect(response.body.error).toEqual("You have not reviewed this product");
+    expect(ReviewController.prototype.getProductReviews).toHaveBeenCalledTimes(1);
+});
+//Example of a unit test for the  DELETE ezelectronics/reviews/:model route
+test("It should return a 200 success code", async () => {
+    jest.spyOn(Authenticator.prototype,"isCustomer").mockImplementation((req,res,next)=>next());
+    jest.spyOn(ReviewController.prototype, "deleteReview").mockResolvedValue(true);
+    const response = await request(app).delete('${baseURL}/reviews/iPhone13');
+    expect(response.status).toBe(200);
+    expect(Authenticator.prototype.isCustomer).toHaveBeenCalledTimes(1);
+    expect(ReviewController.prototype.deleteReview).toHaveBeenCalledTimes(1);
+});
+test("It should return a 404 status code", async () => {
+    jest.spyOn(Authenticator.prototype,"isCustomer").mockImplementation((req,res,next)=>next());
+    jest.spyOn(ReviewController.prototype, "deleteReview").mockRejectedValue(new ProductNotFoundError());
+    const response = await request(app).delete('${baseURL}/reviews/notexisting');
+    expect(response.status).toBe(404);
+    expect(response.body.error).toEqual("Product not found");
+    expect(Authenticator.prototype.isCustomer).toHaveBeenCalledTimes(1);
+    expect(ReviewController.prototype.getProductReviews).toHaveBeenCalledTimes(1);
+});
+test("It should return a 404 status code", async () => {
+    jest.spyOn(Authenticator.prototype,"isCustomer").mockImplementation((req,res,next)=>next());
+    jest.spyOn(ReviewController.prototype, "deleteReview").mockRejectedValue(new NoReviewProductError());
+    const response = await request(app).delete('${baseURL}/reviews/iPhone13');
+    expect(response.status).toBe(404);
+    expect(response.body.error).toEqual("You have not reviewed this product");
+    expect(Authenticator.prototype.isCustomer).toHaveBeenCalledTimes(1);
+    expect(ReviewController.prototype.getProductReviews).toHaveBeenCalledTimes(1);
+});
+//Example of a unit test for the  DELETE ezelectronics/reviews/:model route
+test("It should return a 200 success code", async () => {
+    jest.spyOn(Authenticator.prototype,"isCustomer").mockImplementation((req,res,next)=>next());
+    jest.spyOn(ReviewController.prototype, "deleteReview").mockResolvedValue(true);
+    const response = await request(app).delete('${baseURL}/reviews/iPhone13');
+    expect(response.status).toBe(200);
+    expect(Authenticator.prototype.isCustomer).toHaveBeenCalledTimes(1);
+    expect(ReviewController.prototype.deleteReview).toHaveBeenCalledTimes(1);
+});
+test("It should return a 404 status code", async () => {
+    jest.spyOn(Authenticator.prototype,"isAdminOrManager").mockImplementation((req,res,next)=>next());
+    jest.spyOn(ReviewController.prototype, "deleteReview").mockRejectedValue(new ProductNotFoundError());
+    const response = await request(app).delete('${baseURL}/reviews/notexisting');
+    expect(response.status).toBe(404);
+    expect(response.body.error).toEqual("Product not found");
+    expect(Authenticator.prototype.isCustomer).toHaveBeenCalledTimes(1);
+    expect(ReviewController.prototype.getProductReviews).toHaveBeenCalledTimes(1);
+});
