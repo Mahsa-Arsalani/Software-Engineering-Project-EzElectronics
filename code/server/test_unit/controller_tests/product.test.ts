05aee8b72b50c504195ec161b7239a5e408356bf
LORENZO STEFANONI s330591 <s330591@studenti.polito.it>
2024-06-09 16:29:43 +0200

    product test

--- code/server/test_unit/controller_tests/product.test.ts
+++ code/server/test_unit/controller_tests/product.test.ts
@@ -20,28 +20,28 @@ describe("ProductController unit testing", ()=>{
     const testSmartphone = {
         sellingPrice: 50.00,
         model: "Samsung GalaxyA54",
-        category: "Smartphone",
+        category: Category.SMARTPHONE,
         arrivalDate: null,
         details: null,
         quantity: 50
     }
 
     const testAppliance = {
-        sellingPrice: "10,00",
+        sellingPrice: 10.00,
         model: "Alexa",
-        category: "Appliance",
+        category: Category.APPLIANCE,
         arrivalDate: null,
         details: null,
         quantity: 20
     }
 
     const testLaptop = {
-        sellingPrice: "200,00",
+        sellingPrice: 200.00,
         model: "Hp PC",
-        category: "Laptop",
+        category: Category.LAPTOP,
         arrivalDate: null,
         details: null,
-        quantity: 100
+        quantity: 0
     }
 
     // Example of a unit test for the newModel method of the UserProduct
@@ -72,14 +72,124 @@ describe("ProductController unit testing", ()=>{
         test("It should return 55", async() => {
             jest.spyOn(ProductDAO.prototype, "updateModel").mockResolvedValueOnce(55);
             const controller = new ProductController(); //Create a new instance of the controller
-            //Call the registerProducts method of the controller with the test user object
+            //Call the changeProductQuantity method of the controller with the test user object
             const response = await controller.changeProductQuantity(testSmartphone.model, 5, null);
         
             //Check if the newModel method of the DAO has been called once with the correct parameters
             expect(ProductDAO.prototype.updateModel).toHaveBeenCalledTimes(1);
             expect(ProductDAO.prototype.updateModel).toHaveBeenCalledWith(
                 testSmartphone.model, 5, null);
-            expect(response).toBe(55); //Check if the response is the quantity + 5
+            expect(response).toBe(55); //Check if the response is the quantity, so 50, + 5
+        });
+    });
+
+    describe("sellProduct test cases", ()=>{
+        test("It should return 45", async() => {
+            jest.spyOn(ProductDAO.prototype, "sellModel").mockResolvedValueOnce(45);
+            const controller = new ProductController(); //Create a new instance of the controller
+            //Call the sellProduct method of the controller with the test user object
+            const response = await controller.sellProduct(testSmartphone.model, 5, null);
+        
+            //Check if the newModel method of the DAO has been called once with the correct parameters
+            expect(ProductDAO.prototype.sellModel).toHaveBeenCalledTimes(1);
+            expect(ProductDAO.prototype.sellModel).toHaveBeenCalledWith(
+                testSmartphone.model, 5, null);
+            expect(response).toBe(45); //Check if the response is the quantity, so 50, - 5
+        });
+    });
+
+    describe("sellProduct test cases", ()=>{
+        test("It should return 45", async() => {
+            jest.spyOn(ProductDAO.prototype, "sellModel").mockResolvedValueOnce(45);
+            const controller = new ProductController(); //Create a new instance of the controller
+            //Call the sellProduct method of the controller with the test user object
+            const response = await controller.sellProduct(testSmartphone.model, 5, null);
+        
+            //Check if the newModel method of the DAO has been called once with the correct parameters
+            expect(ProductDAO.prototype.sellModel).toHaveBeenCalledTimes(1);
+            expect(ProductDAO.prototype.sellModel).toHaveBeenCalledWith(
+                testSmartphone.model, 5, null);
+            expect(response).toBe(45); //Check if the response is the quantity, so 50, - 5
+        });
+    });
+
+    describe("getProducts test cases", ()=>{
+        test("it should resolve to a product list", async ()=>{
+            const requested = [new Product( testSmartphone.sellingPrice, 
+                testSmartphone.model, 
+                testSmartphone.category, 
+                testSmartphone.arrivalDate, 
+                testSmartphone.details, 
+                testSmartphone.quantity
+                ), 
+                new Product(testAppliance.sellingPrice, 
+                testAppliance.model, 
+                testAppliance.category, 
+                testAppliance.arrivalDate, 
+                testAppliance.details, 
+                testAppliance.quantity),
+                new Product(testLaptop.sellingPrice, 
+                testLaptop.model, 
+                testLaptop.category, 
+                testLaptop.arrivalDate, 
+                testLaptop.details, 
+                testLaptop.quantity)];
+            jest.spyOn(ProductDAO.prototype, "getAllProducts").mockResolvedValueOnce(requested);
+            const controller = new ProductController();
+            const response = await controller.getProducts(null, null, null);
+            expect(ProductDAO.prototype.getAllProducts).toHaveBeenCalledTimes(1);
+            expect(response).toEqual(requested)
+        });
+    });
+
+    describe("getAvailableProducts test cases", ()=>{
+        test("it should resolve to a product list", async ()=>{
+            const requested = [new Product( testSmartphone.sellingPrice, 
+                testSmartphone.model, 
+                testSmartphone.category, 
+                testSmartphone.arrivalDate, 
+                testSmartphone.details, 
+                testSmartphone.quantity
+                ), 
+                new Product(testAppliance.sellingPrice, 
+                testAppliance.model, 
+                testAppliance.category, 
+                testAppliance.arrivalDate, 
+                testAppliance.details, 
+                testAppliance.quantity)];
+            jest.spyOn(ProductDAO.prototype, "getAllProducts").mockResolvedValueOnce(requested);
+            const controller = new ProductController();
+            const response = await controller.getProducts(null, null, null);
+            expect(ProductDAO.prototype.getAllProducts).toHaveBeenCalledTimes(1);
+            expect(response).toEqual(requested)
+        });
+    });
+
+    describe("deleteAllProdycts test cases", ()=>{
+        test("It should return true", async () => {
+            jest.spyOn(ProductDAO.prototype, "deleteAllProducts").mockResolvedValueOnce(true); //Mock the deleteAllProducts method of the DAO
+            const controller = new ProductController(); //Create a new instance of the controller
+            //Call the deleteProducts method of the controller with the test object
+            const response = await controller.deleteAllProducts();
+        
+            //Check if the deleteAllProducts method of the DAO has been called once with the correct parameters
+            expect(ProductDAO.prototype.deleteAllProducts).toHaveBeenCalledTimes(1);
+            expect(ProductDAO.prototype.deleteAllProducts).toHaveBeenCalledWith();
+            expect(response).toBe(true); //Check if the response is true
+        });
+    });
+
+    describe("deleteProduct test cases", ()=>{
+        test("It should return true", async () => {
+            jest.spyOn(ProductDAO.prototype, "deleteOneProduct").mockResolvedValueOnce(true); //Mock the deleteAllProducts method of the DAO
+            const controller = new ProductController(); //Create a new instance of the controller
+            //Call the deleteProduct method of the controller with the test object
+            const response = await controller.deleteProduct(testAppliance.model);
+        
+            //Check if the deleteOneProduct method of the DAO has been called once with the correct parameters
+            expect(ProductDAO.prototype.deleteOneProduct).toHaveBeenCalledTimes(1);
+            expect(ProductDAO.prototype.deleteOneProduct).toHaveBeenCalledWith(testAppliance.model);
+            expect(response).toBe(true); //Check if the response is true
         });
     });
 });
