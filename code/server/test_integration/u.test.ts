8761692fc30cb6f4c2a287a6e80f7849c4718d6d
Lorenzo Capuano s333279 <s333279@studenti.polito.it>
2024-06-11 10:46:50 +0200

    user integration testing

--- code/server/test_integration/u.test.ts
+++ code/server/test_integration/u.test.ts
@@ -57,6 +57,17 @@ const testAdmin = {
 
 const testAdminUser = new User(testAdmin.username,testAdmin.name,testAdmin.surname,testAdmin.role,null as any, null as any)
 
+const testManager = {
+    username: "testManagerUsername",
+    name: "testManagerName",
+    surname: "testManagerSurname",
+    password: "testManagerPassword",
+    role: Role.MANAGER,
+    address: "testManagerAddress",
+    birthdate: "testManagerBirthdate"
+}
+
+const testManagerUser = new User(testManager.username,testManager.name,testManager.surname,testManager.role,null as any, null as any)
 
 const testCustomer = {
     username: "testCustomerUsername",
@@ -72,6 +83,7 @@ const testCustomerUser = new User(testCustomer.username,testCustomer.name,testCu
 
 let AdminCookie = ""
 let CustomerCookie = ""
+let ManagerCookie = ""
 
 beforeAll(async ()=>{
     await cleanup()
@@ -107,6 +119,14 @@ describe("User integration testing ", ()=>{
 
         test("login as new added user, it should return 200", async ()=>{
             CustomerCookie = await login(testCustomer)
+        })
+
+        test("Add a Manager, it should return 200", async()=>{
+            await postUser(testManager)
+        })
+
+        test("login as new added manager, it should return 200", async ()=>{
+            ManagerCookie = await login(testManager)
         })
 
         test("invalid username, should return 422", async()=>{
@@ -192,17 +212,25 @@ describe("User integration testing ", ()=>{
             //Verify response contains users registered untill now
             expect(body).toContainEqual(testAdminUser)
             expect(body).toContainEqual(testCustomerUser)
+            expect(body).toContainEqual(testManagerUser)
         })
 
-        test("Acces route as custumer, it should return all the existring useres", async()=>{
+        test("Acces route as custumer, it should return 401", async()=>{
             const {body} = await request(app)
                                 .get(baseURL + "/users")
                                 .set("Cookie", CustomerCookie)
                                 .expect(401)
         })
+
+        test("Acces route as Manager, it should return 401", async()=>{
+            const {body} = await request(app)
+                                .get(baseURL + "/users")
+                                .set("Cookie", ManagerCookie)
+                                .expect(401)
+        })
     })
 
-    describe("GET /users/:role",()=>{
+    describe("GET /users/roles/:role",()=>{
         test("Acces route as admin, it should return all the existring useres of a specific role", async()=>{
             const {body} = await request(app)
                                 .get(baseURL + "/users/roles/" + Role.ADMIN)
@@ -212,6 +240,7 @@ describe("User integration testing ", ()=>{
             //Verify response contains users registered untill now
             expect(body).toContainEqual(testAdminUser)
             expect(body).not.toContainEqual(testCustomerUser)
+            expect(body).not.toContainEqual(testManagerUser)
         })
 
         test("Invalid Role, it should return 422", async()=>{
@@ -221,11 +250,86 @@ describe("User integration testing ", ()=>{
                                 .expect(422)
         })
 
-        test("Acces route as custumer, it should return all the existring useres", async()=>{
+        test("Acces route as custumer, it should return 401", async()=>{
             const {body} = await request(app)
                                 .get(baseURL + "/users/roles" + Role.CUSTOMER)
                                 .set("Cookie", CustomerCookie)
                                 .expect(401)
+        })
+
+        test("Acces route as manager, it should return 401", async()=>{
+            const {body} = await request(app)
+                                .get(baseURL + "/users/roles" + Role.MANAGER)
+                                .set("Cookie", ManagerCookie)
+                                .expect(401)
+        })
+    })
+
+    describe("GET /users/username",()=>{
+        test("invalid username, it should return 422", async()=>{
+            //cant test, empty username in params couse an redirection
+            //param are casted as string by default
+        })
+        
+        test("access route as admin and request others own information, it should return 200", async()=>{
+            const {body} = await request(app)
+                                .get(baseURL + "/users/" + testAdminUser.username)
+                                .set("Cookie", AdminCookie)
+                                .expect(200)
+                                
+            //verify is the right user
+            expect(body).toEqual(testAdminUser)
+        })
+
+        test("access route as customer and request others own information, it should return 200", async()=>{
+            const {body} = await request(app)
+                                .get(baseURL + "/users/" + testCustomerUser.username)
+                                .set("Cookie", CustomerCookie)
+                                .expect(200)
+                                
+            //verify is the right user
+            expect(body).toEqual(testCustomerUser)
+        })
+
+        test("access route as manager and request others own information, it should return 200", async()=>{
+            const {body} = await request(app)
+                                .get(baseURL + "/users/" + testManagerUser.username)
+                                .set("Cookie", ManagerCookie)
+                                .expect(200)
+                                
+            //verify is the right user
+            expect(body).toEqual(testManagerUser)
+        })
+
+        test("access route as admin and request others information it should return 200", async()=>{
+            const {body} = await request(app)
+                                .get(baseURL + "/users/" + testCustomerUser.username)
+                                .set("Cookie", AdminCookie)
+                                .expect(200)
+
+            //verify is the right user
+            expect(body).toEqual(testCustomerUser)
+        })
+
+        test("access route as customer and request others information it should return 401", async()=>{
+            const {body} = await request(app)
+                                .get(baseURL + "/users/" + testAdminUser.username)
+                                .set("Cookie", CustomerCookie)
+                                .expect(401)
+        })
+
+        test("access route as manager and request others information it should return 401", async()=>{
+            const {body} = await request(app)
+                                .get(baseURL + "/users/" + testAdminUser.username)
+                                .set("Cookie", ManagerCookie)
+                                .expect(401)
+        })
+
+        test("access route as admin and request non existing user it should return 404", async()=>{
+            const {body} = await request(app)
+                                .get(baseURL + "/users/" + "Anonymous")
+                                .set("Cookie", AdminCookie)
+                                .expect(404)
         })
     })
 
