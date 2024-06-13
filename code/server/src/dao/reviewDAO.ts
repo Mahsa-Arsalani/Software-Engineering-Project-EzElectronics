89295b3080586351c4483d442fbdc4453a284101
MERIC ULUCAY S328899 <s328899@studenti.polito.it>
2024-06-13 15:15:37 +0200

    update review integration test 2

--- code/server/src/dao/reviewDAO.ts
+++ code/server/src/dao/reviewDAO.ts
@@ -2,7 +2,6 @@ import db from "../db/db"
 import { User} from "../components/user"
 import { ProductReview} from "../components/review"
 import { ExistingReviewError, NoReviewProductError} from "../errors/reviewError";
-import ProductDAO from "../dao/productDAO";
 import { ProductNotFoundError } from "../../src/errors/productError";
 /**
  * A class that implements the interaction with the database for all review-related operations.
@@ -17,47 +16,80 @@ class ReviewDAO {
      * @param comment The comment made by the user
      * @returns A Promise that resolves to nothing
      */
-    private ProductDAO: ProductDAO
-/*const createTableSql = 
+
+    initReviewDB():Promise<Boolean>{
+        return new Promise<Boolean>((resolve,reject)=>{
+            const createTableSql = 
                 `CREATE TABLE IF NOT EXISTS reviews (
-                model TEXT PRIMARY KEY,
+                model TEXT,
                 user TEXT,
                 score INTEGER,
                 date TEXT
-                comment TEXT)`;
-
+                comment TEXT
+                PRIMARY KEY(model,user))`;
+                
             // Runs the query
-            db.run(createTableSql, [], (err: Error | null) => { });*/
+            db.run(createTableSql, [], (err: Error | null) => { 
+                if(err)
+                    reject(err)
+                resolve(true)
+            });
+        });
+    }
+deleteTable(): Promise<Boolean> {
+    return new Promise<Boolean>((resolve, reject) => {
+        const sql = "DROP TABLE reviews"
+            db.run(sql, [], (err: Error | null) => {
+                if(err)
+                    reject(err)
+                resolve(true)
+            })
+    })
+}
+        
 addReview(model:string,user:User,score:number,comment:string):Promise<void>{
     return new Promise<void>((resolve,reject)=>{
         try{
+            console.log("A")
             const checkSql1 = "SELECT model FROM products WHERE model = ?";
             db.get(checkSql1, [model], (err: Error | null, row: any) => {
                 if (err) {
+                    console.log("MUST NOT BE HERE1")
                     reject(err);
                 }
                 if (row) {  
+                    console.log("B")
                     const checkSql2 = "SELECT * FROM reviews WHERE model = ? and user = ?";
-                    db.get(checkSql2, [user.username, model], (err: Error | null, row: any) => {
+                    db.get(checkSql2, [model,user.username], (err: Error | null, row: any) => {
                         if (err) {
+                            console.log("MUST NOT BE HERE2")
                             reject(err);
                         }
                         if (row) {
                             reject(new ExistingReviewError());
                         }
-                        const date = new Date().toISOString().split('T')[0];
-                        const insertsql="INSERT INTO reviews(model,user,score,date, comment) VALUES (?,?,?,?,?)";
-                        db.run(insertsql, [model,user.username,score,date,comment], (err: Error | null) => {
-                            if (err) {
-                                reject(err);
-                            }   
-                            resolve();
-                        });
+                        else{
+                            console.log("C")
+                            let date = new Date().toISOString().split('T')[0];
+                            const insertsql="INSERT INTO reviews(model,user,score,date,comment) VALUES(?,?,?,?,?)";
+                            db.run(insertsql, [model,user.username,score,date,comment], (err: Error | null) => {
+                                if (err) {
+                                    console.log("MUST NOT BE HERE3")
+                                    console.log(err.message)
+                                    reject(err);
+                                } 
+                                else{  
+                                    console.log("D")
+                                    resolve();
+                                }
+                            });
+                        }
                     });
                 }
                 else throw new ProductNotFoundError();
             });
         } catch (error) {
+            console.log("MUST NOT BE HERE4")
             reject(error);
         }    
     });
@@ -67,7 +99,7 @@ addReview(model:string,user:User,score:number,comment:string):Promise<void>{
      * @param model The model of the product to get reviews from
      * @returns A Promise that resolves to an array of ProductReview objects
      */
- getProductReviews(model: string):Promise<ProductReview[]>{ 
+getProductReviews(model: string):Promise<ProductReview[]>{ 
     return new Promise<ProductReview[]>((resolve,reject)=>{
         try{
             const sql="SELECT * FROM reviews WHERE model = ?";
